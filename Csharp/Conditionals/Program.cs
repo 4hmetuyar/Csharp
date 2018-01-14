42e13f078f051fa00a096183efd3ea82ef8eaa33
Ahmet Uyar <4hmetuyar@gmail.com>
2018-01-14 22:39:04 +0300

    İf Conditional

---
+++ Csharp/Conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 100;
+
+            if (number == 100)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
