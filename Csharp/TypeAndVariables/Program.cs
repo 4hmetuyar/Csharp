f18c08531e0f864675acb2806b90ef386f87e74f
Ahmet Uyar <4hmetuyar@gmail.com>
2018-01-14 22:23:43 +0300

    Value Types

---
+++ Csharp/TypeAndVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TypeAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+
+            int number1 = 10;
+            Console.WriteLine("Number 1 is {0}", number1);
+            Console.ReadLine();
+
+        }
+    }
+}
