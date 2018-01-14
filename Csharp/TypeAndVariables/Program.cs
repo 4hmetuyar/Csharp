28b8b54baf9d84c9e3bde88abec502d7422b49cf
Ahmet Uyar <4hmetuyar@gmail.com>
2018-01-14 22:29:25 +0300

    int,long,short and byte

--- Csharp/TypeAndVariables/Program.cs
+++ Csharp/TypeAndVariables/Program.cs
@@ -8,8 +8,14 @@ namespace TypeAndVariables
         {
             //Value Types
 
-            int number1 = 10;
+            int number1 = 10;//32 Bit
+            long number2 = 10;//64 Bit
+            short number3 = 10;//16 Bit
+            byte number4 = 10;//8 Bit
             Console.WriteLine("Number 1 is {0}", number1);
+            Console.WriteLine("Number 2 is {0}", number2);
+            Console.WriteLine("Number 3 is {0}", number3);
+            Console.WriteLine("Number 4 is {0}", number4);
             Console.ReadLine();
 
         }
